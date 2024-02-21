92852680a39d184b6b789e9d790347a8eb06873f
marek3289 <>
2024-02-21 15:30:57 +0100

    feat(products): added add new product button

--- client/src/features/products/index.tsx
+++ client/src/features/products/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { SingleProduct } from './components'
 
 interface IProduct {
@@ -64,7 +65,10 @@ export default function Products() {
 
   return (
     <div className='max-w-[1400px] mx-auto p-4 flex flex-col justify-center items-center'>
-      <h1 className='text-2xl font-bold mb-16'>Products</h1>
+      <div className='mb-16 w-full flex items-center justify-around'>
+        <h1 className='text-2xl font-bold'>Products</h1>
+        <Link to='/products/add' className='btn-primary'>Add new product</Link>
+      </div>
       
       <ul className='products-wrapper w-full'>
         {productsList.map((product: IProduct) => (
